079de034fe9a8a188665aab7bdf0dc3be226ae09
willarion <anisimova.marina.n@gmail.com>
2024-03-16 19:49:21 +0200

    added cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
   await app.listen(3001);
 }
 bootstrap();
